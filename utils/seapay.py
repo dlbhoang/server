df42b0a2f24718de03409ccbd4dd28738a0e7380
dlb <142780043+dlbhoang@users.noreply.github.com>
2025-07-01 17:19:26 +0700

    Update seapay.py

--- utils/seapay.py
+++ utils/seapay.py
@@ -1,15 +1,19 @@
 import urllib.parse
 
 SEAPAY_ACCOUNT = "88865298888"     # Số tài khoản nhận tiền
-SEAPAY_BANK_CODE = "tp"         # Mã ngân hàng
+SEAPAY_BANK_CODE = "TPBank"        # Mã ngân hàng đúng theo URL sepay
 
 def generate_vietqr_url(plan):
     try:
+        # Loại bỏ các ký tự không phải số để chuyển sang int
         amount_str = plan.price_vnd.replace(",", "").replace("₫", "").replace(".", "").strip()
         amount = int(amount_str)
+
+        # Mô tả giao dịch
         description = f"Mua gói {plan.name}"
         encoded_des = urllib.parse.quote(description)
 
+        # Tạo URL QR
         return (
             f"https://qr.sepay.vn/img?"
             f"acc={SEAPAY_ACCOUNT}&"
